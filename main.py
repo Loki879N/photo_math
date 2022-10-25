7a29e96589ad5ee383052238c5545279858bbf97
Dedstate <Ted.tukan@gmail.com>
2022-10-25 22:52:19 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+simport os
+import sys
+
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+
+# Сhange working directory
+cur_dir = os.path.dirname(__file__)
+os.chdir(cur_dir)
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("src/MainWindow.ui", self)
+        self.pushButton.clicked.connect(self.clicked)
+
+    def solution(self):
+        ...
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
